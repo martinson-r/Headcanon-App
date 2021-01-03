57025df9a57b073ead897ee0d39df370edd99eb8
Rihana Chang <martinson.r@gmail.com>
2021-01-02 16:38:37 -0800

    troubleshoot CSRF issue

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,7 +3,7 @@ const { check } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 const { handleValidationErrors } = require("../../utils/validation");
-const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
